2c9c9ff0a2d995b5112526770dcf137f932504ce
odiewan <odie@odiesystems.com>
2020-05-06 07:09:15 -0700

    Created Project framework

---
+++ print_folder_preview/Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+        Me.spcMain = New System.Windows.Forms.SplitContainer()
+        CType(Me.spcMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.spcMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'spcMain
+        '
+        Me.spcMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.spcMain.Location = New System.Drawing.Point(0, 0)
+        Me.spcMain.Name = "spcMain"
+        Me.spcMain.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.spcMain.Size = New System.Drawing.Size(800, 450)
+        Me.spcMain.SplitterDistance = 266
+        Me.spcMain.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.spcMain)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.spcMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.spcMain.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents spcMain As SplitContainer
+End Class
